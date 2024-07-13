6fc9a3f2c7286c7952e4774c6f62cdf9456cc587
akm <akm2000@gmail.com>
2024-07-13 11:53:52 +0900

    :sparkles: テストのためにエイリアスを追加

---
+++ aliases.go
@@ -0,0 +1,13 @@
+package goosecond
+
+import (
+	"github.com/pressly/goose/v3"
+)
+
+type (
+	GoMigrationContext = goose.GoMigrationContext
+)
+
+var (
+	AddNamedMigrationContext = goose.AddNamedMigrationContext
+)
